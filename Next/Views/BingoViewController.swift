3385e9c2a6aad736eb7a1c7110d219ff463093b9
DenisAlexandrov <aie-dinis2007@yandex.ru>
2024-06-14 00:24:28 +0300

    bingo

---
+++ Next/Views/BingoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BingoViewController.swift
+//  Next
+//
+//  Created by Денис Александров on 14.06.2024.
+//
+
+import UIKit
+
+class BingoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
